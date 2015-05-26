9dd1925a516b5351e55b776981880793328ce556
thatkawaiiguy <ivanthegreat15@gmail.com>
2015-05-26 10:30:32 -0400

    Add attack graphics

--- src/src/Battle.java
+++ src/src/Battle.java
@@ -8,6 +8,7 @@ import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 import javax.swing.border.Border;
 
 public class Battle extends JFrame implements KeyListener{
@@ -16,14 +17,15 @@ public class Battle extends JFrame implements KeyListener{
 	
 	public JFrame frame;
 
+	public JLabel action;
 	public JLabel selectionBox;
 	public JLabel selectLabel;
 	public JLabel bgr;
 	
-	public JLabel attack1 = new JLabel("Attack1");
-	public JLabel attack2 = new JLabel("Attack2");
-	public JLabel attack3 = new JLabel("Attack3");
-	public JLabel attack4 = new JLabel("Attack4");
+	public JLabel attack1 = new JLabel("ThunderBolt");
+	public JLabel attack2 = new JLabel("Iron Tail");
+	public JLabel attack3 = new JLabel("Sand Attack");
+	public JLabel attack4 = new JLabel("Harden");
 
 	Color borderColor = new Color(223, 234, 216);
 	Color selectorBg = new Color(24, 32, 204);
@@ -55,22 +57,22 @@ public class Battle extends JFrame implements KeyListener{
 	}
 	
 	public void createBoxes() {
-		attack1.setBounds(50, 370, 100, 50);
+		attack1.setBounds(50, 395, 100, 50);
 		attack1.setForeground(Color.WHITE);
 		attack1.setFont(attack1.getFont().deriveFont(16.0f));
 		frame.add(attack1);
 		
-		attack2.setBounds(50, 430, 100, 50);
+		attack2.setBounds(50, 445, 100, 50);
 		attack2.setForeground(Color.WHITE);
 		attack2.setFont(attack2.getFont().deriveFont(16.0f));
 		frame.add(attack2);
 		
-		attack3.setBounds(340, 370, 100, 50);
+		attack3.setBounds(340, 395, 100, 50);
 		attack3.setForeground(Color.WHITE);
 		attack3.setFont(attack3.getFont().deriveFont(16.0f));
 		frame.add(attack3);
 		
-		attack4.setBounds(340, 430, 100, 50);
+		attack4.setBounds(340, 445, 100, 50);
 		attack4.setForeground(Color.WHITE);
 		attack4.setFont(attack4.getFont().deriveFont(16.0f));
 		frame.add(attack4);
@@ -85,52 +87,76 @@ public class Battle extends JFrame implements KeyListener{
 		selectionBox.setBorder(border);
 		selectionBox.setBackground(selectorBg);
 		selectionBox.setOpaque(true);
-		selectionBox.setBounds(0, 344, 860, 166);
+		selectionBox.setBounds(0, 377, 949, 136);
 		frame.add(selectionBox);
 	}
-
+	
+	/// MAKE SOUND LOOOOP
+	public void attackSelect(JLabel label){
+		frame.remove(attack1);
+		frame.remove(attack2);
+		frame.remove(attack3);
+		frame.remove(attack4);
+		frame.remove(selectionBox);
+		frame.remove(selectLabel);
+		frame.repaint();
+		
+		action = new JLabel("You used " + label.getText() + "!");
+		action.setForeground(Color.white);
+		action.setFont(action.getFont().deriveFont(18.0f));
+		action.setBounds(80, 345, 800, 200);
+		frame.add(action);
+		frame.add(selectionBox);
+		JLayeredPane layered = new JLayeredPane();
+		frame.setContentPane(layered);
+		layered.add(action, 0);
+		layered.add(selectionBox, 1);
+		layered.add(bgr, 2);
+		frame.setVisible(true);
+	}
+	
 	@Override
 	public void keyPressed(KeyEvent key) {
-		if(key.getKeyCode() == KeyEvent.VK_DOWN){
+		if(key.getKeyCode() == KeyEvent.VK_ENTER){
+			if(selectLabel.getX() == attack1.getX() - 45)
+				attackSelect(attack1);
+			if(selectLabel.getY() == attack2.getY())
+				attackSelect(attack2);
+			if(selectLabel.getX() == attack3.getX() - 45)
+				attackSelect(attack3);
+			if(selectLabel.getY() == attack4.getY())
+				attackSelect(attack4);
+		}
+		else if(key.getKeyCode() == KeyEvent.VK_DOWN){
 			if(selectLabel.getX() == attack1.getX() - 45)
 				selectLabel.setLocation(attack2.getX() - 45, attack2.getY());
 			if(selectLabel.getX() == attack3.getX() - 45)
 				selectLabel.setLocation(attack4.getX() - 45, attack4.getY());
 		}
-
-		if(key.getKeyCode() == KeyEvent.VK_RIGHT){
+		else if(key.getKeyCode() == KeyEvent.VK_RIGHT){
 			if(selectLabel.getX() == attack1.getX() - 45)
 				selectLabel.setLocation(attack3.getX() - 45, attack3.getY());
 			if(selectLabel.getX() == attack2.getX() - 45)
 				selectLabel.setLocation(attack4.getX() - 45, attack4.getY());
 		}
-
-		if(key.getKeyCode() == KeyEvent.VK_LEFT){
+		else if(key.getKeyCode() == KeyEvent.VK_LEFT){
 			if(selectLabel.getX() == attack3.getX() - 45)
 				selectLabel.setLocation(attack1.getX() - 45, attack1.getY());
 			if(selectLabel.getX() == attack4.getX() - 45)
 				selectLabel.setLocation(attack2.getX() - 45, attack2.getY());
 		}
-
-		if(key.getKeyCode() == KeyEvent.VK_UP){
+		else if(key.getKeyCode() == KeyEvent.VK_UP){
 			if(selectLabel.getX() == attack2.getX() - 45)
 				selectLabel.setLocation(attack1.getX() - 45, attack1.getY());
 			if(selectLabel.getX() == attack4.getX() - 45)
 				selectLabel.setLocation(attack3.getX() - 45, attack3.getY());
 		}
-		frame.setVisible(true);
-			
+		frame.setVisible(true);	
 	}
+	
+	@Override
+	public void keyReleased(KeyEvent arg0) {}
 
 	@Override
-	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void keyTyped(KeyEvent arg0) {}
 }
